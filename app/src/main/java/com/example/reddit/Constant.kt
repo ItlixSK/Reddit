be1e035352fbd95eabecc96fa7d77aa367eaee50
Sergey <109138599+ItlixSK@users.noreply.github.com>
2022-07-17 17:45:05 +0300

    Stage-3

--- app/src/main/java/com/example/reddit/Constant.kt
+++ app/src/main/java/com/example/reddit/Constant.kt
@@ -1,3 +1,4 @@
 package com.example.reddit
 
+lateinit var MAIN: MainActivity
 const val BASE_URL = "https://www.reddit.com/"
--- app/src/main/java/com/example/reddit/MainActivity.kt
+++ app/src/main/java/com/example/reddit/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.reddit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.reddit.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private var binding:ActivityMainBinding? = null
+    private val mBinding get() = binding!!
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_splash)
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(5000)
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(mBinding.root)
+            MAIN = this@MainActivity
+            navController = Navigation.findNavController(MAIN,R.id.navHost)
+
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
